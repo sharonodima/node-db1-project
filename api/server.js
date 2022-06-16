d18112204f5cd2af4d7b8e11e8ac6cc732c7a3c4
Sharon Odima <odimasharon@gmail.com>
2022-06-16 07:36:50 -0400

    Sharon Odima Unit4.2Project1 initial commit

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 
 const server = express();
+const accountsRouter = require(("./accounts/accounts-router"))
 
 server.use(express.json());
+server.use("/api/accounts", accountsRouter)
+
+server.use("*", (req, res) => {
+    res.status(404).json({
+        messsage: "Not found"
+    })
+})
 
 module.exports = server;
