3d1030a34eab1257107818d936d9d767f563547d
Sharon Odima <odimasharon@gmail.com>
2022-06-16 09:19:43 -0400

    Sharon Odima completed accounts-middleware.js in Unit4.2Project2

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,45 +1,45 @@
 const router = require('express').Router()
+const md = require("./accounts-middleware")
+const Account = require("./accounts-model")
 
-router.get('/', (req, res, next) => {
+
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
   try{
-    res.json([{}, {}])
-  } catch(err) {
-    next({status: 422, message: "This is Horrible"})
-  }
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-  try{
-
+    const accounts = await Account.getAll()
+    res.json(accounts)
   } catch(err) {
     next(err)
   }
 })
 
-router.post('/', (req, res, next) => {
+router.get('/:id', md.checkAccountId, async (req, res, next) => {
+  // DO YOUR MAGIC
+  res.json(req.account)
+})
+
+router.post('/', md.checkAccountPayload, md.checkAccountNameUnique, (req, res, next) => {
   // DO YOUR MAGIC
   try{
-
+    res.json("Post account")
   } catch(err) {
     next(err)
   }
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', md.checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
   try{
-
+    res.json("Update account")
   } catch(err) {
     next(err)
   }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', md.checkAccountId, md.checkAccountPayload, md.checkAccountNameUnique, (req, res, next) => {
   // DO YOUR MAGIC
   try{
-
+    res.json("Delete account")
   } catch(err) {
     next(err)
   }
